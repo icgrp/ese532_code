37e30cc11e84b24af779524146440fe46156e200
Syed Ahmed <stahmed@seas.upenn.edu>
2020-09-15 12:56:48 -0400

    Add coarse grain

---
+++ hw3/assignment/coarse_grain/App.h
@@ -0,0 +1,13 @@
+#ifndef SRC_APP_H_
+#define SRC_APP_H_
+
+#define HEIGHT (540)
+#define WIDTH (960)
+
+void Scale(const unsigned char * Input, unsigned char * Output, int Y_Start_Idx, int Y_End_Idx);
+void Filter(const unsigned char * Input, unsigned char * Output);
+void Differentiate(const unsigned char * Input, unsigned char * Output);
+int Compress(const unsigned char * Input, unsigned char * Output);
+
+#endif
+
---
+++ hw3/assignment/coarse_grain/Scale.cpp
@@ -0,0 +1,8 @@
+#include "App.h"
+
+void Scale(const unsigned char * Input, unsigned char * Output, int Y_Start_Idx, int Y_End_Idx)
+{
+  for (int Y = Y_Start_Idx; Y < Y_End_Idx; Y += 2)
+    for (int X = 0; X < WIDTH; X += 2)
+      Output[(Y / 2) * WIDTH / 2 + (X / 2)] = Input[Y * WIDTH + X];
+}
