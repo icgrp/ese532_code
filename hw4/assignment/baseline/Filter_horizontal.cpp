bf6b1f87cf3b397b2b8c710e158c46cf32962a52
Syed Ahmed <stahmed@seas.upenn.edu>
2020-09-22 16:32:09 -0400

    Separate out filter

---
+++ hw4/assignment/baseline/Filter_horizontal.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#define INPUT_HEIGHT (270)
+#define INPUT_WIDTH (480)
+
+#define FILTER_LENGTH (7)
+
+#define OUTPUT_HEIGHT (INPUT_HEIGHT - (FILTER_LENGTH - 1))
+#define OUTPUT_WIDTH (INPUT_WIDTH - (FILTER_LENGTH - 1))
+
+unsigned Coefficients[] = {2, 15, 62, 98, 62, 15, 2};
+
+void Filter_horizontal(const unsigned char *Input, unsigned char *Output)
+{
+  for (int Y = 0; Y < INPUT_HEIGHT; Y++)
+    for (int X = 0; X < OUTPUT_WIDTH; X++)
+    {
+      unsigned int Sum = 0;
+      for (int i = 0; i < FILTER_LENGTH; i++)
+        Sum += Coefficients[i] * Input[Y * INPUT_WIDTH + X + i];
+      Output[Y * OUTPUT_WIDTH + X] = Sum >> 8;
+    }
+}
