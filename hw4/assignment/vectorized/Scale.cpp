1b22540504b8a4993fc984aa580c2e45bec1df49
Syed Ahmed <stahmed@seas.upenn.edu>
2020-09-23 18:10:06 -0400

    Rearrange src

--- hw4/assignment/vectorized/Scale.cpp
+++
@@ -1,9 +0,0 @@
-#define HEIGHT (540)
-#define WIDTH (960)
-
-void Scale(const unsigned char *Input, unsigned char *Output)
-{
-  for (int Y = 0; Y < HEIGHT; Y += 2)
-    for (int X = 0; X < WIDTH; X += 2)
-      Output[(Y / 2) * WIDTH / 2 + (X / 2)] = Input[Y * WIDTH + X];
-}
