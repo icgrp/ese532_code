8f9a59538113b80cb941e0106e83b148edaedc19
Syed Ahmed <stahmed@seas.upenn.edu>
2020-09-25 13:20:35 -0400

    Add unoptimized intrinsics implementation

--- hw4/assignment/src/Filter.cpp
+++ hw4/assignment/src/Filter.cpp
@@ -26,7 +26,49 @@ void Filter_horizontal(const unsigned char *Input, unsigned char *Output)
 #include <arm_neon.h>
 void Filter_vertical(const unsigned char *Input, unsigned char *Output)
 {
-  // put your vector code here
+  // modify the following code
+  uint16x8_t Coef0 = vdupq_n_u16(Coefficients[0]);
+  uint16x8_t Coef1 = vdupq_n_u16(Coefficients[1]);
+  uint16x8_t Coef2 = vdupq_n_u16(Coefficients[2]);
+  uint16x8_t Coef3 = vdupq_n_u16(Coefficients[3]);
+  for (int X = 0; X < OUTPUT_WIDTH; X += 8)
+  {
+    uint8x16_t Data0 = vld1q_u8(Input + 0 * OUTPUT_WIDTH + X);
+   	uint8x16_t Data1 = vld1q_u8(Input + 1 * OUTPUT_WIDTH + X);
+   	uint8x16_t Data2 = vld1q_u8(Input + 2 * OUTPUT_WIDTH + X);
+   	uint8x16_t Data3 = vld1q_u8(Input + 3 * OUTPUT_WIDTH + X);
+   	uint8x16_t Data4 = vld1q_u8(Input + 4 * OUTPUT_WIDTH + X);
+   	uint8x16_t Data5 = vld1q_u8(Input + 5 * OUTPUT_WIDTH + X);
+    for (int Y = 0; Y < OUTPUT_HEIGHT; Y++)
+    {
+   	  uint8x16_t Data6 = vld1q_u8(Input + (Y + 6) * OUTPUT_WIDTH + X);
+   	  uint16x8_t Sum0H = vaddl_u8(vget_high_u8(Data0), vget_high_u8(Data6));
+   	  uint16x8_t Sum1H = vaddl_u8(vget_high_u8(Data1), vget_high_u8(Data5));
+   	  uint16x8_t Sum2H = vaddl_u8(vget_high_u8(Data2), vget_high_u8(Data4));
+   	  uint16x8_t SumH = vmulq_u16(Sum0H, Coef0);
+   	  SumH = vmlaq_u16(SumH, Sum1H, Coef1);
+   	  SumH = vmlaq_u16(SumH, Sum2H, Coef2);
+   	  SumH = vmlaq_u16(SumH, vmovl_u8(vget_high_u8(Data3)), Coef3);
+   	  SumH = vshrq_n_u16(SumH, 8);
+   	  uint8x8_t ResultH = vmovn_u16(SumH);
+   	  uint16x8_t Sum0L = vaddl_u8(vget_low_u8(Data0), vget_low_u8(Data6));
+   	  uint16x8_t Sum1L = vaddl_u8(vget_low_u8(Data1), vget_low_u8(Data5));
+   	  uint16x8_t Sum2L = vaddl_u8(vget_low_u8(Data2), vget_low_u8(Data4));
+   	  uint16x8_t SumL = vmulq_u16(Sum0L, Coef0);
+   	  SumL = vmlaq_u16(SumL, Sum1L, Coef1);
+   	  SumL = vmlaq_u16(SumL, Sum2L, Coef2);
+   	  SumL = vmlaq_u16(SumL, vmovl_u8(vget_low_u8(Data3)), Coef3);
+   	  SumL = vshrq_n_u16(SumL, 8);
+   	  uint8x8_t ResultL = vmovn_u16(SumL);
+   	  vst1q_u8(Output + Y * OUTPUT_WIDTH + X, vcombine_u8(ResultL, ResultH));
+   	  Data0 = Data1;
+   	  Data1 = Data2;
+   	  Data2 = Data3;
+   	  Data3 = Data4;
+   	  Data4 = Data5;
+   	  Data5 = Data6;
+    }
+  }
 }
 #else
 void Filter_vertical(const unsigned char *Input, unsigned char *Output)
