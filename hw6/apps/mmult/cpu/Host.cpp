e7c1e52601b2f49170292873127828271dbb312f
Syed Ahmed <stahmed@seas.upenn.edu>
2020-10-19 16:57:51 -0400

    Modify cpu code

--- hw6/apps/mmult/cpu/Host.cpp
+++ hw6/apps/mmult/cpu/Host.cpp
@@ -7,47 +7,41 @@
 #include <unistd.h>
 #include <vector>
 
-constexpr int N = 32;
-
-constexpr int PIPELINE_DEPTH_MIN = 1;
-constexpr int PIPELINE_DEPTH_MAX = 4;
-constexpr int PIPELINE_DEPTH_DEFAULT = 4;
-
-static void init_arrays(float *A, float *B, unsigned int num_tests) {
-  for (int c = 0; c < num_tests; c++) {
+static void init_arrays(float *A, float *B) {
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
-        A[c * N * N + i * N + j] = 1 + i * N + j;
-        B[c * N * N + i * N + j] = rand() % (N * N);
+        A[i * N + j] = rand() % 512;
+        B[i * N + j] = rand() % 512;
       }
     }
-  }
 }
 
-void mmult_cpu(float *A, float *B, float *C, unsigned int num_tests) {
-  for (int c = 0; c < num_tests; c++)
-    for (int row = 0; row < N; row++) {
-      for (int col = 0; col < N; col++) {
-        float result = 0.0;
-        for (int k = 0; k < N; k++) {
-          result += A[c * N * N + row * N + k] * B[c * N * N + k * N + col];
-        }
-        C[c * N * N + row * N + col] = result;
+void mmult_cpu(float *A, float *B, float *C) {
+  for (int row = 0; row < N; row++) {
+    for (int col = 0; col < N; col++) {
+      float result = 0.0;
+      for (int k = 0; k < N; k++) {
+        result += A[row * N + k] * B[k * N + col];
       }
+      C[row * N + col] = result;
     }
+  }
 }
 
 int main(int argc, char *argv[]) {
   EventTimer timer;
   float *A, *B, *C;
-  unsigned int num_tests = 8192;
+  // matrix size, N x N
+  unsigned int N = 1024;
 
+  // pass from the command line if you want
+  // to see how long things take
   if (argc == 2) {
-    num_tests = atoi(argv[1]);
+    N = atoi(argv[1]);
   }
-  A = (float *)malloc(num_tests * N * N * sizeof(float));
-  B = (float *)malloc(num_tests * N * N * sizeof(float));
-  C = (float *)malloc(num_tests * N * N * sizeof(float));
+  A = (float *)malloc(N * N * sizeof(float));
+  B = (float *)malloc(N * N * sizeof(float));
+  C = (float *)malloc(N * N * sizeof(float));
   if (!A || !B || !C) {
     if (A)
       free(A);
@@ -58,15 +52,15 @@ int main(int argc, char *argv[]) {
     return 2;
   }
 
-  std::cout << "Running " << num_tests << " " << N << "x" << N
+  std::cout << "Running " << N << "x" << N
             << " floating point mmult on cpu..." << std::endl;
 
   timer.add("Creating arrays: A, B, C");
-  init_arrays(A, B, num_tests);
+  init_arrays(A, B);
 
   timer.add("Running mmult_cpu");
-  mmult_cpu(A, B, C, num_tests);
-  store_data("output_cpu.bin", C, num_tests * N * N * sizeof(float));
+  mmult_cpu(A, B, C);
+  store_data("output_cpu.bin", C, N * N * sizeof(float));
 
   free(A);
   free(B);
