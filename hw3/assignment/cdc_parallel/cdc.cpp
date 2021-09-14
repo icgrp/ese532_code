0f52243193b05d91545e8f525f8538bd234c5342
Syed Tousif Ahmed <stahmed@seas.upenn.edu>
2021-09-14 08:51:57 -0400

    Add parallel cdc

---
+++ hw3/assignment/cdc_parallel/cdc.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+#define WIN_SIZE 16
+#define PRIME 3
+#define MODULUS 256
+#define TARGET 0
+#define NUM_THREADS 4
+
+uint64_t hash_func(unsigned char *input, unsigned int pos)
+{
+	// put your hash function implementation here
+}
+
+void cdc(unsigned char *buff, unsigned int buff_size)
+{
+	// put your cdc implementation here
+
+}
+
+void test_cdc( const char* file )
+{
+	FILE* fp = fopen(file,"r" );
+	if(fp == NULL ){
+		perror("fopen error");
+		return;
+	}
+
+	fseek(fp, 0, SEEK_END); // seek to end of file
+	int file_size = ftell(fp); // get current file pointer
+	fseek(fp, 0, SEEK_SET); // seek back to beginning of file
+
+	unsigned char* buff = (unsigned char *)malloc((sizeof(unsigned char) * file_size ));	
+	if(buff == NULL)
+	{
+		perror("not enough space");
+		fclose(fp);
+		return;
+	}
+
+	int bytes_read = fread(&buff[0],sizeof(unsigned char),file_size,fp);
+
+	// parallelize cdc over 4 threads here
+	// cdc(buff, file_size);
+
+    free(buff);
+    return;
+}
+
+int main()
+{
+	test_cdc("../data/prince.txt");
+	return 0;
+}
--- hw3/assignment/pipeline_4_cores/Scale.cpp
+++
@@ -1,8 +0,0 @@
-#include "App.h"
-
-void Scale(const unsigned char *Input, unsigned char *Output)
-{
-    for (int Y = 0; Y < INPUT_HEIGHT_SCALE; Y += 2)
-        for (int X = 0; X < INPUT_WIDTH_SCALE; X += 2)
-            Output[(Y / 2) * INPUT_WIDTH_SCALE / 2 + (X / 2)] = Input[Y * INPUT_WIDTH_SCALE + X];
-}
