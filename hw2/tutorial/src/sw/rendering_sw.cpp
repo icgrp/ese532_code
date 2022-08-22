f5d74be86f24784744614e9b54e39157c765610a
ept221 <ezra150@gmail.com>
2022-08-22 19:06:25 -0400

    refactored stopwatch in hw2

--- hw2/tutorial/src/sw/rendering_sw.cpp
+++ hw2/tutorial/src/sw/rendering_sw.cpp
@@ -10,42 +10,8 @@
 
 /*======================UTILITY FUNCTIONS========================*/
 #include <cstdint>
-#include <chrono>
 #include <iostream>
-
-class stopwatch
-{
-  public:
-    double total_time, calls;
-    std::chrono::time_point<std::chrono::high_resolution_clock> start_time, end_time;
-    stopwatch() : total_time(0), calls(0) {};
-
-    inline void reset() {
-      total_time = 0;
-      calls = 0;
-    }
-
-    inline void start() {
-      start_time = std::chrono::high_resolution_clock::now();
-      calls++;
-    };
-
-    inline void stop() {
-      end_time = std::chrono::high_resolution_clock::now();
-      auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time-start_time).count();
-      total_time += static_cast<double>(elapsed);
-    };
-
-    // return latency in ns
-    inline double latency() {
-      return total_time;
-    };
-
-    // return latency in ns
-    inline double avg_latency() {
-      return (total_time / calls);
-    };
-};
+#include "stopwatch.h"
 
 // Determine whether three vertices of a trianlgLe
 // (x0,y0) (x1,y1) (x2,y2) are in clockwise order by Pineda algorithm
---
+++ hw2/tutorial/src/sw/stopwatch.h
@@ -0,0 +1,40 @@
+#ifndef STOPWATCH_H_
+#define STOPWATCH_H_
+
+#include <chrono>
+
+class stopwatch
+{
+  public:
+    double total_time, calls;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time, end_time;
+    stopwatch() : total_time(0), calls(0) {};
+
+    inline void reset() {
+      total_time = 0;
+      calls = 0;
+    }
+
+    inline void start() {
+      start_time = std::chrono::high_resolution_clock::now();
+      calls++;
+    };
+
+    inline void stop() {
+      end_time = std::chrono::high_resolution_clock::now();
+      auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time-start_time).count();
+      total_time += static_cast<double>(elapsed);
+    };
+
+    // return latency in ns
+    inline double latency() {
+      return total_time;
+    };
+
+    // return latency in ns
+    inline double avg_latency() {
+      return (total_time / calls);
+    };
+};
+
+#endif
