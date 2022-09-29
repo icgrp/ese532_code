ab25a54af9ff9db5580cd0d0c652551efab631b8
djp <sologoal@naver.com>
2022-09-28 23:21:08 -0400

    define LOOP_SIZE in hw4 example

--- hw4/assignment/neon_example/Example.cpp
+++ hw4/assignment/neon_example/Example.cpp
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <iostream>
 #include <arm_neon.h>
 #include "Stopwatch.h"
+
+#define LOOP_SIZE 1024
 
 void add3_NEON(uint8_t* neon_data){
 	/* Create the vector with our data. */
 	uint8x16_t data;
 	uint8x16_t three = vmovq_n_u8(3);
-	for (int i = 0; i < 8; i++) {
+	for (int i = 0; i < LOOP_SIZE/16; i++) {
 		data = vld1q_u8(neon_data);
 		data = vaddq_u8(data, three);
 		vst1q_u8(neon_data, data);
@@ -17,32 +19,41 @@ void add3_NEON(uint8_t* neon_data){
 }
 
 void add3_c(unsigned int *data){
-	for(int i=0;i<128;i++){
+	for(int i=0;i<LOOP_SIZE;i++){
 		data[i] += 3;
 	}
 }
 
 void print_NEON (uint8_t* data, char* name) {
 	printf ("%s:\n", name);
-	for (int i = 0; i < 128; i++) {
+	for (int i = 0; i < LOOP_SIZE; i++) {
 		printf ("%02d\n", data[i]);
 	}
 }
 
 void print_C (unsigned int *data, char* name) {
     printf ("%s:\n", name);
-    for (int i = 0; i < 128; i++) {
+    for (int i = 0; i < LOOP_SIZE; i++) {
 	    printf ("%02d\n", data[i]);
     }
 }
 
+bool compare_results (unsigned int *c_data, uint8_t *neon_data){
+    for (int i = 0; i < LOOP_SIZE; i++){
+        if((uint8_t)c_data[i] != neon_data[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
 int main () {
     /* Create custom arbitrary data. */
-    uint8_t neon_data[128];
-    unsigned int c_data[128];
-    for(int i=0;i<128;i++){
-    	neon_data[i] = i;
-    	c_data[i] = i;
+    uint8_t neon_data[LOOP_SIZE];
+    unsigned int c_data[LOOP_SIZE];
+    for(int i=0;i<LOOP_SIZE;i++){
+    	neon_data[i] = i%128;
+    	c_data[i] = i%128;
     }
 
 
@@ -61,8 +72,10 @@ int main () {
     printf("C add3 takes %fns \n", timer.latency());
 
     /* Compare the latency and output */
-    print_NEON(neon_data, "data_with_neon");
-    print_C(c_data, "data_without_neon");
+    //print_NEON(neon_data, "data_with_neon");
+    //print_C(c_data, "data_without_neon");
+    bool equal = compare_results(c_data, neon_data);
+    std::cout << "Results are " << (equal ? "equal" : "NOT equal") << std::endl;
 
     return EXIT_SUCCESS;
 }
