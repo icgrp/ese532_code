e1795e652760f0198d7877a9abb4c8469b5c3757
Ezra Thomas <ept@seas.upenn.edu>
2022-10-12 12:37:13 -0400

    updated host code to work with enqueueMapBuffer

--- hw6/apps/mmult/fpga/Host.cpp
+++ hw6/apps/mmult/fpga/Host.cpp
@@ -16,137 +16,136 @@
 #include "MMult.h"
 #include "Utilities.h"
 
-static void init_arrays(float *A[NUM_MAT],  
-                        float *B[NUM_MAT])
+static void init_arrays(float *A[NUM_MAT], float *B[NUM_MAT])
 {
-     for (int m = 0; m < NUM_MAT; m++) {
-    	for (int c = 0; c < CHUNKS; c++) {
-          for (int i = 0; i < N; i++) {
-               for (int j = 0; j < N; j++) {
+    for (int m = 0; m < NUM_MAT; m++)
+    {
+        for (int c = 0; c < CHUNKS; c++)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
                     A[m][ c * N * N + i * N + j] = 1+i*N+j;
                     B[m][ c * N * N + i * N + j] = rand() % (N * N);
-               }
-          }
-    	}
-     }
+                }
+            }
+        }
+    }
 }
 
-int main(int argc, char *argv[]) {
-  EventTimer timer1, timer2;
-  timer1.add("Main program");
+int main(int argc, char *argv[])
+{
+    EventTimer timer1, timer2;
+    timer1.add("Main program");
 
-  std::cout << "Running " << CHUNKS << "x" <<NUM_TESTS << " iterations of " << N << "x" << N
-               << " task pipelined floating point mmult..." << std::endl;
-  // ------------------------------------------------------------------------------------
-  // Step 1: Initialize the OpenCL environment
-  // ------------------------------------------------------------------------------------
-  timer2.add("OpenCL Initialization");
-  cl_int err;
-  std::string binaryFile = argv[1];
-  unsigned fileBufSize;
-  std::vector<cl::Device> devices = get_xilinx_devices();
-  devices.resize(1);
-  cl::Device device = devices[0];
-  cl::Context context(device, NULL, NULL, NULL, &err);
-  char *fileBuf = read_binary_file(binaryFile, fileBufSize);
-  cl::Program::Binaries bins{{fileBuf, fileBufSize}};
-  cl::Program program(context, devices, bins, NULL, &err);
-  cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE, &err);
-  cl::Kernel krnl_mmult(program, "mmult_fpga", &err);
+    std::cout << "Running " << CHUNKS << "x" <<NUM_TESTS << " iterations of " << N << "x" << N
+    << " task pipelined floating point mmult..." << std::endl;
+    // ------------------------------------------------------------------------------------
+    // Step 1: Initialize the OpenCL environment
+     // ------------------------------------------------------------------------------------
+    timer2.add("OpenCL Initialization");
+    cl_int err;
+    std::string binaryFile = argv[1];
+    unsigned fileBufSize;
+    std::vector<cl::Device> devices = get_xilinx_devices();
+    devices.resize(1);
+    cl::Device device = devices[0];
+    cl::Context context(device, NULL, NULL, NULL, &err);
+    char *fileBuf = read_binary_file(binaryFile, fileBufSize);
+    cl::Program::Binaries bins{{fileBuf, fileBufSize}};
+    cl::Program program(context, devices, bins, NULL, &err);
+    cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE, &err);
+    cl::Kernel krnl_mmult(program, "mmult_fpga", &err);
 
-  // ------------------------------------------------------------------------------------
-  // Step 2: Create buffers and initialize test values
-  // ------------------------------------------------------------------------------------
-  timer2.add("Allocate contiguous OpenCL buffers");
-  // Create the buffers and allocate memory
-  cl::Buffer A_buf[NUM_MAT];
-  cl::Buffer B_buf[NUM_MAT];
-  cl::Buffer C_buf[NUM_MAT];
-  float *A[NUM_MAT], *B[NUM_MAT], *C[NUM_MAT];
-  size_t elements_per_iteration = CHUNKS * N * N;
-  size_t bytes_per_iteration = elements_per_iteration * sizeof(float);
+    // ------------------------------------------------------------------------------------
+    // Step 2: Create buffers and initialize test values
+    // ------------------------------------------------------------------------------------
+    timer2.add("Allocate contiguous OpenCL buffers");
 
-  for (int m = 0; m < NUM_MAT; m++) {
-    A[m] = (float *)malloc(bytes_per_iteration);
-    B[m] = (float *)malloc(bytes_per_iteration);
-    C[m] = (float *)malloc(bytes_per_iteration);
-    if (!A[m] || !B[m] || !C[m]) {
-      if (A[m])
-        free(A[m]);
-      if (B[m])
-        free(B[m]);
-      if (C[m])
-        free(C[m]);
-      return 2;
+    size_t elements_per_iteration = CHUNKS * N * N;
+    size_t bytes_per_iteration = elements_per_iteration * sizeof(float);
+
+    cl::Buffer a_buf[NUM_MAT];
+    cl::Buffer b_buf[NUM_MAT];
+    cl::Buffer c_buf[NUM_MAT];
+    for(int i = 0; i < NUM_MAT; i++)
+    {
+        a_buf[i] = cl::Buffer(context, CL_MEM_READ_ONLY, bytes_per_iteration, NULL, &err);
+        b_buf[i] = cl::Buffer(context, CL_MEM_READ_ONLY, bytes_per_iteration, NULL, &err);
+        c_buf[i] = cl::Buffer(context, CL_MEM_WRITE_ONLY, bytes_per_iteration, NULL, &err);
     }
 
-    A_buf[m] = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-                     bytes_per_iteration, A[m], &err);
-    B_buf[m] = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-                     bytes_per_iteration, B[m], &err);
-    C_buf[m] = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
-                        bytes_per_iteration, C[m], &err);
-  }
-  
-  timer2.add("Populating buffer inputs");
-  init_arrays(A, B);
-  
-  // ------------------------------------------------------------------------------------
-  // Step 3: Run the kernel
-  // ------------------------------------------------------------------------------------
-
-  timer2.add("Running kernel");
-  std::vector<cl::Event> write_events;
-  for (int i = 0; i < NUM_TESTS; i++) {
-    std::vector<cl::Event> exec_events, read_events;
-    cl::Event write_ev, exec_ev, read_ev;
-
-    krnl_mmult.setArg(0, A_buf[i%NUM_MAT]);
-    krnl_mmult.setArg(1, B_buf[i%NUM_MAT]);
-    krnl_mmult.setArg(2, C_buf[i%NUM_MAT]);
-    if(i == 0){
-        q.enqueueMigrateMemObjects({A_buf[i%NUM_MAT], B_buf[i%NUM_MAT]}, 0 /* 0 means from host*/, NULL,
-                              &write_ev);
-    } else {
-
-        q.enqueueMigrateMemObjects({A_buf[i%NUM_MAT], B_buf[i%NUM_MAT]}, 0 /* 0 means from host*/, &write_events,
-                              &write_ev);
-        write_events.pop_back();
+    float *a[NUM_MAT];
+    float *b[NUM_MAT];
+    float *c[NUM_MAT];
+    for(int i = 0; i < NUM_MAT; i++)
+    {
+        a[i] = (float*)q.enqueueMapBuffer(a_buf[i], CL_TRUE, CL_MAP_WRITE, 0, bytes_per_iteration);
+        b[i] = (float*)q.enqueueMapBuffer(b_buf[i], CL_TRUE, CL_MAP_WRITE, 0, bytes_per_iteration);
+        c[i] = (float*)q.enqueueMapBuffer(c_buf[i], CL_TRUE, CL_MAP_READ, 0, bytes_per_iteration);
     }
 
-    write_events.push_back(write_ev);
-    q.enqueueTask(krnl_mmult, &write_events, &exec_ev);
-    exec_events.push_back(exec_ev);
-    q.enqueueMigrateMemObjects({C_buf[i%NUM_MAT]}, CL_MIGRATE_MEM_OBJECT_HOST, &exec_events, &read_ev);
-    read_events.push_back(read_ev);
-  }
+    timer2.add("Populating buffer inputs");
+    init_arrays(a, b);
 
-  q.finish();
-  
-  // ------------------------------------------------------------------------------------
-  // Step 4: Release Allocated Resources
-  // ------------------------------------------------------------------------------------
-  timer2.add("Writing output to output_fpga.bin");
-  FILE *file = fopen("output_fpga.bin", "wb");
-  for (int m = 0; m < NUM_MAT; m++) {
-    fwrite(C[m], 1, bytes_per_iteration, file);
-    free(A[m]);
-    free(B[m]);
-    free(C[m]);
-  }
-  fclose(file);
+    // ------------------------------------------------------------------------------------
+    // Step 3: Run the kernel
+    // ------------------------------------------------------------------------------------
 
-  delete[] fileBuf;
-  
-  
-  timer2.finish();
-  std::cout << "--------------- Key execution times ---------------"
-            << std::endl;
-  timer2.print();
+    timer2.add("Running kernel");
+    std::vector<cl::Event> write_events;
+    for (int i = 0; i < NUM_TESTS; i++)
+    {
+        std::vector<cl::Event> exec_events, read_events;
+        cl::Event write_ev, exec_ev, read_ev;
 
-  timer1.finish();
-  std::cout << "--------------- Total time ---------------"
-            << std::endl;
-  timer1.print();
-  return 0;
+        krnl_mmult.setArg(0, a_buf[i%NUM_MAT]);
+        krnl_mmult.setArg(1, b_buf[i%NUM_MAT]);
+        krnl_mmult.setArg(2, c_buf[i%NUM_MAT]);
+        if(i == 0)
+        {
+            q.enqueueMigrateMemObjects({a_buf[i%NUM_MAT], b_buf[i%NUM_MAT]}, 0 /* 0 means from host*/, NULL, &write_ev);
+        }
+        else
+        {
+            q.enqueueMigrateMemObjects({a_buf[i%NUM_MAT], b_buf[i%NUM_MAT]}, 0 /* 0 means from host*/, &write_events,
+            &write_ev);
+            write_events.pop_back();
+        }
+
+        write_events.push_back(write_ev);
+        q.enqueueTask(krnl_mmult, &write_events, &exec_ev);
+        exec_events.push_back(exec_ev);
+        q.enqueueMigrateMemObjects({c_buf[i%NUM_MAT]}, CL_MIGRATE_MEM_OBJECT_HOST, &exec_events, &read_ev);
+        read_events.push_back(read_ev);
+    }
+
+    q.finish();
+
+    // ------------------------------------------------------------------------------------
+    // Step 4: Release Allocated Resources
+    // ------------------------------------------------------------------------------------
+
+    timer2.add("Writing output to output_fpga.bin");
+    FILE *file = fopen("output_fpga.bin", "wb");
+
+    for (int i = 0; i < NUM_MAT; i++)
+    {
+      fwrite(c[i], 1, bytes_per_iteration, file);
+    }
+    fclose(file);
+
+    delete[] fileBuf;
+
+    timer2.finish();
+    std::cout << "--------------- Key execution times ---------------"
+    << std::endl;
+    timer2.print();
+
+    timer1.finish();
+    std::cout << "--------------- Total time ---------------"
+    << std::endl;
+    timer1.print();
+    return 0;
 }
