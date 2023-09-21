9ff57e744ce58dc6fe0e8b4bab16d98cdba06d25
ept221 <ept@seas.upenn.edu>
2023-09-21 14:28:42 -0400

    updating hw4

--- hw4/assignment/src/Scale.cpp
+++ hw4/assignment/src/Scale.cpp
@@ -1,9 +1,23 @@
 #define HEIGHT (540)
 #define WIDTH (960)
+#include <stdio.h>
 
+
+#ifdef VECTORIZED
+#include <arm_neon.h>
 void Scale(const unsigned char *Input, unsigned char *Output)
 {
-  for (int Y = 0; Y < HEIGHT; Y += 2)
-    for (int X = 0; X < WIDTH; X += 2)
-      Output[(Y / 2) * WIDTH / 2 + (X / 2)] = Input[Y * WIDTH + X];
+  // Write your NEON accelerated version of Scale here.
 }
+#else
+void Scale(const unsigned char *Input, unsigned char *Output)
+{
+  for(int Y = 0; Y < HEIGHT; Y += 2)
+  {
+    for(int X = 0; X < WIDTH; X += 2)
+    {
+      Output[(Y / 2) * (WIDTH / 2) + (X / 2)] = Input[(Y * WIDTH) + X];
+    }
+  }
+}
+#endif
